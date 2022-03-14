0e215c83482d8c1502c5c5dad5a6504e5b7fe921
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-03-14 18:54:07 -0300

    Update.

--- inicio.py
+++ inicio.py
@@ -1,3 +1,21 @@
 print("Aprendendo Python para trabalhar com dados!")
 
+print("Aprendendo", "Python","para", "trabalhar", "com", "dados!", sep="")
+
 print("Aprendendo", "Python", "para", "trabalhar", "com", "dados!", sep = "-")
+
+print("Aprendendo Python para trabalhar com dados! esse é o dia 1", end = "\n")
+
+cargo = "back-end"
+type(cargo)
+
+dia = 1
+type(dia)
+
+print("Aprendendo Python para trabalhar com", cargo, "! Esse é o dia", dia)
+
+
+subst = "Python"
+verbo = "é"
+adjetivo = "fantástico"
+print(subst, verbo, adjetivo, sep="_", end="!\n")
