2bf929de9488b3d6f6473f4f72978a6fa071666a
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-03-15 06:02:32 -0300

    Improving readability in conditions.

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,12 +7,14 @@ numero_secreto = 42
 chute = input("Digite o seu número: ")
 numero = int(chute)
 
-print("Você digitou ", chute)
+acertou = numero == numero_secreto
+maior   = numero > numero_secreto
+menor   = numero < numero_secreto
 
-if(numero_secreto == numero):
+if(acertou):
     print("Voce acertou")
 else:
-    if(numero > numero_secreto):
+    if(maior):
         print("Voce errou! O seu chute foi maior que o numero secreto.")
-    elif (numero < numero_secreto):
+    elif (menor):
         print("Voce errou! O seu chute foi menor que o numero secreto.")
