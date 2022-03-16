dda37aaa5268234627444c58377523ab8370be6c
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-03-16 06:25:35 -0300

    Testing functions

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,18 +3,26 @@ print("Bem vindo ao jogo de adivinhação!")
 print("***********************************")
 
 numero_secreto = 42
+tentativas = 3
+rodada = 1
 
-chute = input("Digite o seu número: ")
-numero = int(chute)
+while(rodada <= tentativas):
+    print("Rodada {} de {}". format(rodada, tentativas))
+    chute = input("Digite o seu número: ")
+    numero = int(chute)
 
-acertou = numero == numero_secreto
-maior   = numero > numero_secreto
-menor   = numero < numero_secreto
+    acertou = numero == numero_secreto
+    maior   = numero > numero_secreto
+    menor   = numero < numero_secreto
 
-if(acertou):
-    print("Voce acertou")
-else:
-    if(maior):
-        print("Voce errou! O seu chute foi maior que o numero secreto.")
-    elif (menor):
-        print("Voce errou! O seu chute foi menor que o numero secreto.")
+    if(acertou):
+        print("Voce acertou")
+    else:
+        if(maior):
+            print("Voce errou! O seu chute foi maior que o numero secreto.")
+        elif (menor):
+            print("Voce errou! O seu chute foi menor que o numero secreto.")
+
+    rodada = rodada + 1
+
+print("Fim de jogo!")
